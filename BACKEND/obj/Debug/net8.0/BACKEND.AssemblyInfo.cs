91ac25879bb6ae1004cc2007ad67cb7ee76c9034
jerald783 <jeraldtulibao783@gmail.com>
2026-09-11 11:42:49 +0800

    Update agent and user headers

--- BACKEND/obj/Debug/net8.0/BACKEND.AssemblyInfo.cs
+++ BACKEND/obj/Debug/net8.0/BACKEND.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BACKEND")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee02c464b520276d0728215c15690ae286306c43")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f515d6bec60730c6c12be48bf0e4f68e10784abb")]
 [assembly: System.Reflection.AssemblyProductAttribute("BACKEND")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BACKEND")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
